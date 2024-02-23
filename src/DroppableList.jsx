95547ff464f2935e0cd9c30456a9222818691a65
suspiciousRaccoon <spablocabral@gmail.com>
2024-02-23 01:31:35 -0300

    feat: make DroppableList sortable

--- src/DroppableList.jsx
+++ src/DroppableList.jsx
@@ -1,14 +1,24 @@
 import { useDroppable } from "@dnd-kit/core";
 import {
   SortableContext,
+  useSortable,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import React from "react";
 import SortableItem from "./SortableItem";
+import { CSS } from "@dnd-kit/utilities";
 
 const DroppableList = (props) => {
   const { id, list } = props;
-  const { isOver, setNodeRef } = useDroppable({ id });
+  const { isOver, setNodeRef: setNodeRefDroppable } = useDroppable({ id });
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef: setNodeRefSortable,
+    transform,
+    transition,
+  } = useSortable({ id: props.id });
 
   const style = {
     color: isOver ? "green" : undefined,
@@ -18,9 +28,16 @@ const DroppableList = (props) => {
     flex: 1,
     minHeight: "50px",
     minWidth: "50px",
+    transform: CSS.Transform.toString(transform),
+    transition,
   };
   return (
-    <div>
+    <div
+      key={id}
+      id={id}
+      ref={setNodeRefSortable}
+      {...attributes}
+      {...listeners}>
       <h1>{list.name}</h1>
       <SortableContext
         id={id}
